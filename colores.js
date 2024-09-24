f247fbe8e2712044c504697cf9b3ae76816b3720
Chasky91 <david_c91@outlook.com>
2024-09-24 19:03:40 -0300

    uml clases

---
+++ colores.js
@@ -0,0 +1,5 @@
+
+import chalk from 'chalk'
+
+console.log(chalk.rgb(25, 165, 0)("Este texto es de color naranja (RGB)"))
+console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'))
---
+++ cuentaBancaria.js
@@ -0,0 +1,49 @@
+class CuentaBancaria {
+    // Atributo estático
+    static numeroCuentas = 0
+  
+    // Constructor para inicializar los atributos
+    constructor(saldo, numero) {
+      this.saldo = saldo       
+      this.numero = numero     
+      CuentaBancaria.numeroCuentas++ 
+  
+    // Métodos públicos
+  
+    // Devuelve el saldo actual
+    getSaldo() {
+      return this.saldo
+    }
+  
+    // Ingresa una cantidad en la cuenta
+    ingresar(cantidad) {
+      if (cantidad > 0) {
+        this.saldo += cantidad
+      } else {
+        console.log("Cantidad inválida para ingresar.");
+      }
+    }
+  
+    // Retira una cantidad de la cuenta
+    retirar(cantidad) {
+      if (cantidad > 0 && cantidad <= this.saldo) {
+        this.saldo -= cantidad
+      } else {
+        console.log("Cantidad inválida para retirar.")
+      }
+    }
+  
+    // Devuelve el número total de cuentas
+    static getNumeroCuentas() {
+      return CuentaBancaria.numeroCuentas;
+    }
+  }
+  
+  // Ejemplo de uso
+  let cuenta1 = new CuentaBancaria(1000, "12345");
+  console.log(cuenta1.getSaldo());    // 1000
+  cuenta1.ingresar(500);
+  console.log(cuenta1.getSaldo());    // 1500
+  cuenta1.retirar(200);
+  console.log(cuenta1.getSaldo());    // 1300
+  console.log(CuentaBancaria.getNumeroCuentas()); // 1
